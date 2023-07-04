f3fad5de7bde55787d74f39fda9508f534185778
Alston Tang <github@thm64.com>
2023-07-04 16:37:42 +0000

    copy ssh key

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ set -x
 useradd -rm -s /bin/bash -g root -G sudo "${SSH_USER}"
 echo "${SSH_USER}:${SSH_PASSWORD}" | chpasswd
 
+mkdir -p /home/${SSH_USER}/.ssh
+cp /mnt/git-key/* /home/${SSH_USER}/.ssh
+
 /usr/sbin/sshd -D
