49784648ac865044cdfc463984ae46d17c829dac
Alston <github@thm64.com>
2023-07-05 09:18:34 +0000

    run as ssh user

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ cp /mnt/git-key/* /home/${SSH_USER}/.ssh
 chown ${SSH_USER}:root /home/${SSH_USER}/.ssh/*
 
 if [ -n "${GIT_EMAIL}" ]; then
-  git config --global user.email "${GIT_EMAIL}"
+  sudo -u ${SSH_USER} git config --global user.email "${GIT_EMAIL}"
 fi
 if [ -n "${GIT_NAME}" ]; then
-  git config --global user.name "${GIT_NAME}"
+  sudo -u ${SSH_USER} git config --global user.name "${GIT_NAME}"
 fi
 
 /usr/sbin/sshd -D "${@}"
